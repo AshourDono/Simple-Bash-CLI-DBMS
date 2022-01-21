bcf9f2bb26a37ebd92b99f749e6faf290262c5de
AliTaarek <eng.alii.tarek@gmail.com>
2022-01-21 20:34:05 +0200

    fucking update is done

---
+++ updateTable.sh
@@ -0,0 +1,42 @@
+function updateTable {
+  echo -e "Enter Table Name: \c"
+  read tableName
+  metaTable="."
+  echo -e "Enter Condition Column name: \c"
+  read field
+  metaTable="$metaTable$tableName"
+  fid=$(awk -F: '{if($1=="'$field'") print NR}'  dbms/$dbname/$metaTable)
+  #fnumber=$(awk -F: '{if($1=="'$fid'") print NR}'  dbms/$dbname/$metaTable)
+  if [[ $fid == "" ]]
+  then
+    echo "Not Found"
+    tablesMenu
+  else
+    echo -e "Enter Condition Value: \c"
+    read value
+    resultrow=$(awk -F: '{if ($'$fid'=="'$value'") print NR}'  dbms/$dbname/$tableName)
+    if [[ $resultrow == "" ]]
+    then
+      echo "Value Not Found"
+      tablesMenu
+    else
+      echo -e "Enter FIELD name to set: \c"
+      read setField
+      setFieldNumber=$(awk -F: '{if($1=="'$setField'") print NR}'  dbms/$dbname/$metaTable)
+      #setFidNumber=$(awk 'BEGIN{FS=":"}{for(i=1;i<=NR;i++){if($1=="'$setFid'") print $i}}'  dbms/$dbname/$metaTable)
+      if [[ $setField == "" ]]
+      then
+        echo "Not Found"
+        tablesMenu
+      else
+        echo -e "Enter new Value to set: \c"
+        read newValue
+        oldValue=$(awk -F: '{if(NR=='$resultrow')print $'$setFieldNumber'}'  dbms/$dbname/$tableName)
+        echo $oldValue
+        sed -i 's/'$oldValue'/'$newValue'/g' dbms/$dbname/$tableName 
+        echo "Row Updated Successfully"
+        tablesMenu
+      fi
+    fi
+  fi
+}
